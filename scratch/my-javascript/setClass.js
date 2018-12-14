b486a664dbcbf21eb214bc0b62e36023d38bc42f
Robel Debesay <robi7111989@gmail.com>
2018-12-14 07:40:03 -0800

    edited the  js file to the html file

--- scratch/my-javascript/setClass.js
+++ scratch/my-javascript/setClass.js
@@ -4,17 +4,7 @@ function greet(name){
   console.log('Hello,' + name);
 }
 
-function setClassTip(event){
-  let el = document.getElementById('mypara');
-  el.className = 'tip';
-}
-
-function setClassWarning(){
-  let el = document.getElementById('mypara')
-  el.className = 'warning';
-}
-
-function ClearClass(){
-  let el = document.getElementById('mypara')
-  el.className = '';
-}
+ function setClass(event, className) {
+   let el = document.getElementById('mypara')
+   el.className = className;
+ }
