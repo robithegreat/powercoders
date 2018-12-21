1898ce9b84a0a9fbab2f661bda4d6b9ce6812134
Robel Debesay <robi7111989@gmail.com>
2018-12-21 06:51:43 -0800

    ma first interact with the web using javascript

--- scratch/my-javascript/setClass.js
+++ scratch/my-javascript/setClass.js
@@ -4,7 +4,21 @@ function greet(name){
   console.log('Hello,' + name);
 }
 
- function setClass(event, className) {
-   let el = document.getElementById('mypara')
-   el.className = className;
+ function setClass(className) {
+     document.getElementById('mypara').className = className;
  }
+
+ document.addEventListener('DOMContentLoaded', function (event) {
+     document.getElementById('make-warning').addEventListener('click', function (event) {
+         setClass( 'warning');
+     });
+
+     document.getElementById('make-tip').addEventListener('click', function (event) {
+         setClass( 'tip');
+     });
+
+     document.getElementById('make-normal').addEventListener('click', function (event) {
+         setClass('');
+     });
+ });
+
