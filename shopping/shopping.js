77451c40c82be8be41aa0460fa8d9f6dc39cac5f
Robel Debesay <robi7111989@gmail.com>
2019-01-07 07:57:25 -0800

    Iinitial selection for the shopping list
    entering the ananimos function in js

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -2,6 +2,26 @@ document.addEventListener('DOMContentLoaded',function (event) {
     document.querySelector('button').addEventListener('click', function (event) {
         let inputBox = document.getElementById('item');
         console.log(inputBox.value);
+
     });
 
 });
+
+
+function createNewListItem(itemName) {
+
+    let li = document.createElement('li');
+
+    let spann = document.createElement('span');
+
+    spannText = document.createTextNode(itemName);
+    spann.appendChild(spannText);
+
+    let button = document.createElement('button');
+    buttonText = document.createTextNode('Delete');
+    button.appendChild(buttonText);
+
+    li.appendChild(spann);
+    li.appendChild(button);
+    return li;
+}
