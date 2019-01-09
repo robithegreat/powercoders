cd687d8e5009831511aa42b88b6e4180a6db4b5f
Robel Debesay <robi7111989@gmail.com>
2019-01-08 16:38:50 -0800

    Iinitial selection for the shopping list
    adding and deleting shopping list items using js function and manipulate DOM

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,27 +1,29 @@
-document.addEventListener('DOMContentLoaded',function (event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        let inputBox = document.getElementById('item');
-        console.log(inputBox.value);
-
-    });
-
-});
-
 
 function createNewListItem(itemName) {
-
     let li = document.createElement('li');
 
     let spann = document.createElement('span');
-
-    spannText = document.createTextNode(itemName);
-    spann.appendChild(spannText);
+    spann.textContent = itemName;
+    li.appendChild(spann);
 
     let button = document.createElement('button');
-    buttonText = document.createTextNode('Delete');
-    button.appendChild(buttonText);
-
-    li.appendChild(spann);
+    button.textContent = 'Delete';
     li.appendChild(button);
+
+    button.addEventListener('click', function (event) {
+        li.remove();
+        console.log('Delete button is clicked:' + itemName);
+    });
+    document.querySelector('ul').appendChild(li);
     return li;
-}
+};
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('button').addEventListener('click', function(event) {
+        let inputBox = document.getElementById('item');
+        console.log(inputBox.value);
+        let li = createNewListItem(inputBox.value);
+        let ul=document.querySelector('ul');
+        ul.appendChild(li);
+    });
+});
