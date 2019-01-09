89a9eadd57d89877a62a7e4e99062aa86f34c8ff
Robel Debesay <robi7111989@gmail.com>
2019-01-08 16:57:32 -0800

    Iinitial selection for the shopping list

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,29 +1,39 @@
-
 function createNewListItem(itemName) {
     let li = document.createElement('li');
 
-    let spann = document.createElement('span');
+    let spann = document.createElement('spann');
+
     spann.textContent = itemName;
+
     li.appendChild(spann);
 
     let button = document.createElement('button');
+
     button.textContent = 'Delete';
+
     li.appendChild(button);
 
     button.addEventListener('click', function (event) {
         li.remove();
+
         console.log('Delete button is clicked:' + itemName);
     });
-    document.querySelector('ul').appendChild(li);
-    return li;
-};
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    document.querySelector('button').addEventListener('click', function(event) {
+    document.querySelector('ul').appendChild(li);
+
+    return li;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.querySelector('button').addEventListener('click', function (event) {
         let inputBox = document.getElementById('item');
+
         console.log(inputBox.value);
+
         let li = createNewListItem(inputBox.value);
-        let ul=document.querySelector('ul');
+
+        let ul = document.querySelector('ul');
+
         ul.appendChild(li);
     });
 });
