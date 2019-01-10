ddccfa924173bff532f914725ab16859ff9a104c
Robel Debesay <robi7111989@gmail.com>
2019-01-10 05:39:38 -0800

    Iinitial selection for the shopping list
    i added a function that allow you to add an item by presing enter

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,39 +1,28 @@
 function createNewListItem(itemName) {
     let li = document.createElement('li');
-
     let spann = document.createElement('spann');
-
     spann.textContent = itemName;
-
     li.appendChild(spann);
-
     let button = document.createElement('button');
-
     button.textContent = 'Delete';
-
     li.appendChild(button);
-
     button.addEventListener('click', function (event) {
         li.remove();
-
-        console.log('Delete button is clicked:' + itemName);
     });
 
     document.querySelector('ul').appendChild(li);
-
     return li;
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
     document.querySelector('button').addEventListener('click', function (event) {
-        let inputBox = document.getElementById('item');
+    });
 
-        console.log(inputBox.value);
-
-        let li = createNewListItem(inputBox.value);
-
-        let ul = document.querySelector('ul');
-
-        ul.appendChild(li);
+    document.querySelector('input').addEventListener('keyup',function (event) {
+        if (event.key === 'Enter') {
+            let inputBox = document.getElementById('item');
+            let li = createNewListItem(inputBox.value);
+            document.querySelector('ul').appendChild(li);
+        }
     });
 });
