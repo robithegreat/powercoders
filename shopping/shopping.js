6c852325a0e7ed05b611e2d76d0ba80437e133c7
Robel Debesay <robi7111989@gmail.com>
2019-01-21 13:55:40 -0800

    Iinitial selection for the shopping list
    entering css styling and deleteall button in javascript to clear all list items

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,33 +1,79 @@
+/**
+ * Creates and returns an 'li' element for  inclusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
+
 function createNewListItem(itemName) {
-    let li = document.createElement('li');
-    let spann = document.createElement('spann');
-    spann.textContent = itemName;
-    li.appendChild(spann);
-    let button = document.createElement('button');
-    button.textContent = 'Delete';
-    li.appendChild(button);
-    button.addEventListener('click', function (event) {
-        li.remove();
+    const listItem = document.createElement('li');
+    const span = document.createElement('p');
+
+    span.innerText = itemName;
+    listItem.appendChild(span);
+
+    const deleteButton = document.createElement('button');
+    const text = document.createTextNode('Delete');
+    deleteButton.addEventListener('click', function (event) {
+
+        listItem.remove();
     });
 
-    document.querySelector('ul').appendChild(li);
-    return li;
+    deleteButton.appendChild(text);
+    listItem.appendChild(deleteButton);
+
+    return listItem;
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        let inputBox = document.getElementById('item');
-        let li = createNewListItem(inputBox.value);
-        document.querySelector('ul').appendChild(li);
-        inputBox.value = '';
+    /**widget that the use typesson item in to.*/
+    const inputBox = document.getElementById('item');
+    const shoppingList = document.querySelector('ul');
+    const addItemButton = document.querySelector('#add');
+    addItemButton.disabled = false;
+    inputBox.focus();
+    clearButton.disabled = false;
+
+    document.getElementById('add').addEventListener('click', function (event) {
+        if (inputBox.value.trim() !== '') {
+            shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
+            inputBox.value = '';
+            addItemButton.disabled = true;
+
+        }
+        inputBox.focus();
     });
 
-    document.querySelector('input').addEventListener('keyup',function (event) {
-        if (event.key === 'Enter') {
-            let inputBox = document.getElementById('item');
-            let li = createNewListItem(inputBox.value);
-            document.querySelector('ul').appendChild(li);
-            inputBox.value = '';
+    document.querySelector('input').addEventListener('keyup', function (event) {
+        const trimmedValue = inputBox.value.trim();
+        addItemButton.disabled=trimmedValue==='';
+
+        if (trimmedValue === '') {
+            return;
+
         }
+        if (event.key !== 'Enter') {
+            return;
+        }
+
+        shoppingList.appendChild(createNewListItem(trimmedValue));
+        inputBox.value = '';
+        addItemButton.disabled = true;
+        clearButton.disabled = false;
+        inputBox.focus();
     });
+
+    document.getElementById('clearButton').addEventListener('click',function(event){
+        let x = document.querySelectorAll('li');
+        x.forEach(function (el) {
+           el.remove();
+            clearButton.disabled = true;
+            inputBox.focus();
+        });
+    });
+
+
 });
+
+
+
